68a5da3be1613ee35a61a4e8ff2159d614a2d3b2
Anusuya Dutt Purohit <purohitansu7@gmail.com>
2026-09-15 16:31:09 +0530

    Create comprehensive HTML sitemap page to improve AdSense approval and link it in the global footer

---
+++ fix-footer.mjs
@@ -0,0 +1,9 @@
+﻿import fs from 'fs';
+let content = fs.readFileSync('src/components/Footer.astro', 'utf8');
+
+content = content.replace(
+  '<a href="/sitemap-index.xml" class="hover:text-blue-600 transition-colors">Sitemap</a>',
+  '<a href="/sitemap" class="hover:text-blue-600 transition-colors">Sitemap</a>'
+);
+
+fs.writeFileSync('src/components/Footer.astro', content);
